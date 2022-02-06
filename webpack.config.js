61cd9b8d3c234226802858258c9abb00dd05a9ea
Michael Li <thisislimichael@gmail.com>
2022-02-05 16:54:27 -0800

    modularized js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -14,6 +16,10 @@ module.exports = {
           $: "jquery",
           jQuery: "jquery"
         }),
+
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 
